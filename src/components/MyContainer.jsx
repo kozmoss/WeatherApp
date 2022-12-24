a2ebad6380fa4e299874ba18cb529fdb53045f58
Samet Buzcu <buzcu_7@hotmail.com>
2022-12-24 16:14:03 +0300

    commit

--- src/components/MyContainer.jsx
+++ src/components/MyContainer.jsx
@@ -15,7 +15,7 @@ const MyContainer = () => {
     <div className="container">
     <header className="header p-4 text-center">
       <h2 className="text-uppercase text-white" style={{ letterSpacing: 3 }}>
-        weather <span className="fw-bold">forecast</span>
+        weatherr <span className="fw-bold">forecastt</span>
       </h2>
     </header>
     <Search setShowMap={setShowMap}/>
